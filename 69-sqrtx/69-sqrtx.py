0862673bddc35f8317495930bd74ed32da7bee65
Zach Seever <51473778+zseever@users.noreply.github.com>
2022-09-25 02:11:40 -0700

    Time: 92 ms (27.26%), Space: 13.8 MB (56.29%) - LeetHub

--- 69-sqrtx/69-sqrtx.py
+++ 69-sqrtx/69-sqrtx.py
@@ -2,8 +2,22 @@ class Solution:
     def mySqrt(self, x: int) -> int:
         if x == 0:
             return 0
-        for num in range(0,x+1):
-            if num*num == x:
-                return num
-            elif num*num > x:
-                return num-1
+        left = 0
+        right = x
+        while True:
+            mid = math.ceil((left+right)/2)
+            if mid * mid == x:
+                return mid
+            elif right-left == 1:
+                return left
+            elif mid * mid > x:
+                right = mid
+            else:
+                left = mid
+        
+        
+        # for num in range(0,x+1):
+        #     if num*num == x:
+        #         return num
+        #     elif num*num > x:
+        #         return num-1

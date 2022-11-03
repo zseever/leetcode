aacdc852f41b79618079d9f7d43f4b87951b1c60
Zach Seever <51473778+zseever@users.noreply.github.com>
2022-11-03 05:16:09 -0700

    Time: 4140 ms (5.02%), Space: 49.8 MB (17.08%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.js
+++ 0344-reverse-string/0344-reverse-string.js
@@ -3,12 +3,21 @@
  * @return {void} Do not return anything, modify s in-place instead.
  */
 var reverseString = function(s) {
-    let stack = []
-    while (s.length) {
-        stack.push(s.shift())
+    // let stack = []
+    // while (s.length) {
+    //     stack.push(s.shift())
+    // }
+    // while (stack.length) {
+    //     s.push(stack.pop())
+    // }
+    let l = 0
+    let r = s.length-1
+    
+    while (l < r) {
+        s.splice(r+1,0,s[l])
+        s.shift()
+        r--
     }
-    while (stack.length) {
-        s.push(stack.pop())
-    }
+    
     
 };

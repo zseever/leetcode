82c4fcd0560d74f94317af665a6525f51e86aca2
Zach Seever <51473778+zseever@users.noreply.github.com>
2022-11-03 05:04:12 -0700

    Time: 202 ms (30.30%), Space: 49.1 MB (22.19%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.js
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.js
@@ -9,4 +9,27 @@ var sortedSquares = function(nums) {
     }
     squaredNums.sort((a,b) => a-b)
     return squaredNums
+    
+    // let stack = []
+    // for (let i = 0; i < nums.length; i++) {
+    //     if (nums[i] < 0) {
+    //         stack.push(nums[i]**2)
+    //         nums.shift()
+    //         i--
+    //     } else if (nums[i] > 0) {
+    //         nums[i] = nums[i]**2
+    //         while (nums[i] > stack[stack.length-1]) {
+    //             nums.splice(i,0,stack.pop())
+    //             i++
+    //         }
+    //     }
+    //     if (!nums.length) {
+    //         while (stack.length) {
+    //             nums.push(stack.pop())
+    //         }
+    //         i = nums.length
+    //     }
+    // }
+    // return nums
+    
 };

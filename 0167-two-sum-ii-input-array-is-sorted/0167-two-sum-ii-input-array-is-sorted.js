845d7b7b7071267b80dc1416b6a83bec077599b3
Zach Seever <51473778+zseever@users.noreply.github.com>
2022-11-04 04:18:03 -0700

    Time: 61 ms (98.15%), Space: 43.2 MB (31.49%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.js
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.js
@@ -4,18 +4,28 @@
  * @return {number[]}
  */
 var twoSum = function(numbers, target) {
-    let foundResults = {}
-    let answer
-    for (let i = 0; i < numbers.length; i++) {
-        let resultMatch = target - numbers[i]
-        if (foundResults[numbers[i]]) {
-            answer = [foundResults[numbers[i]], i + 1]
-        } else {
-            foundResults[resultMatch] = i + 1
-        }
+//     let foundResults = {}
+//     let answer
+//     for (let i = 0; i < numbers.length; i++) {
+//         let resultMatch = target - numbers[i]
+//         if (foundResults[numbers[i]]) {
+//             answer = [foundResults[numbers[i]], i + 1]
+//         } else {
+//             foundResults[resultMatch] = i + 1
+//         }
         
+//     }
+//     return answer
+    let left = 0
+    let right = numbers.length-1
+    let pointerSum = numbers[left] + numbers[right]
+    while (pointerSum !== target) {
+        if (pointerSum > target) {
+            right--
+        } else if (pointerSum < target) {
+            left++
+        }
+        pointerSum = numbers[left] + numbers[right]
     }
-    // console.log(foundResults)
-    return answer
-    
+    return [left+1, right+1]
 };

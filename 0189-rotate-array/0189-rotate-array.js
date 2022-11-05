03f4ec317d4c6c223e282f5bc6e146d59b6e0a3c
Zach Seever <51473778+zseever@users.noreply.github.com>
2022-11-05 04:18:03 -0700

    Time: 178 ms (40.17%), Space: 51.7 MB (63.87%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function(nums, k) {
+    while (k > nums.length) {
+        k -= nums.length
+    }
+    let slicedNums = nums.splice(nums.length-k,k)
+    console.log(slicedNums)
+    nums.unshift(...slicedNums)
+};

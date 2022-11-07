a9e8b22305d8dff680988cd90eaac03f09064b89
Zach Seever <51473778+zseever@users.noreply.github.com>
2022-11-06 16:44:26 -0800

    Time: 152 ms (48.04%), Space: 51.8 MB (12.99%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    const storedVals = {};
+    for (i = 0; i < nums.length; i++) {
+        if (storedVals[nums[i]]) {
+            return true
+        } else {
+            storedVals[nums[i]] = true;
+        }
+    }
+    return false
+};

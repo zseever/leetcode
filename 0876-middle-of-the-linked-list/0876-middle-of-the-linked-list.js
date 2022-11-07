9478dda807d37b4744978bcf480d1c136813c091
Zach Seever <51473778+zseever@users.noreply.github.com>
2022-11-06 17:03:49 -0800

    Time: 73 ms (82.14%), Space: 43.4 MB (6.51%) - LeetHub

---
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var middleNode = function(head) {
+    let node = head
+    let length = 1
+    while (node.next) {
+        length++
+        node = node.next
+    }
+    let midpoint = (length % 2 === 0) ? length/2 + 1 : Math.ceil(length/2)
+    let curNode = 1
+    node = head
+    while (node.next) {
+        if (curNode === midpoint) {
+            break
+        }
+        console.log(node.val)
+        node = node.next
+        curNode++
+        
+    }
+    return node
+};

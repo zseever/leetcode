2eca8149427942d22d719a60a0aa934a984ad734
Zach Seever <51473778+zseever@users.noreply.github.com>
2022-11-06 16:37:08 -0800

    Time: 54 ms (99.61%), Space: 41.9 MB (62.58%) - LeetHub

--- 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.js
+++ 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.js
@@ -4,9 +4,12 @@
  * @return {number}
  */
 var countOdds = function(low, high) {
-    let oddCtr = 0
-    for (let i=low; i <= high; i++) {
-        if (i%2 !== 0) oddCtr++ 
-    }
-    return oddCtr
+    // let oddCtr = 0
+    // for (let i=low; i <= high; i++) {
+    //     if (i%2 !== 0) oddCtr++ 
+    // }
+    if (high % 2 === 0) high--
+    if (low % 2 === 0) low++
+    return (high-low)/2 + 1
+    // return oddCtr
 };

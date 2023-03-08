9fee40cc164e8834cff0ed6b024c5f120d85864a
Zach Seever <51473778+zseever@users.noreply.github.com>
2023-03-07 16:20:28 -0800

    Time: 68 ms (28.23%), Space: 41.6 MB (90.22%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -5,20 +5,12 @@
  */
 var strStr = function(haystack, needle) {
     let left = 0
-    // let right = 0
     while (left < haystack.length) {
-            console.log('left value: '+left)
-            console.log(haystack.slice(left,left+needle.length))
-            console.log(needle === haystack.slice(left,left+needle.length))
         if (needle === haystack.slice(left,left+needle.length)) {
             return left
         } else {
             left++
         }
-
-        // if (needle === haystack.slice(left,right+1)) {
-        //     return left
-        // }
     }
     return -1
     

6b62a848ab2156aaf552cc77d1cfde937842e421
Zach Seever <51473778+zseever@users.noreply.github.com>
2023-03-11 03:24:45 -0800

    Time: 66 ms (84.76%), Space: 44.4 MB (35.02%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {character}
+ */
+var findTheDifference = function(s, t) {
+    let hash = {}
+    for (let i=0; i < s.length; i++) {
+        hash[s[i]] = hash[s[i]] ? hash[s[i]]+1 : 1
+    }
+    for (let i=0; i < t.length; i++) {
+        hash[t[i]]--
+    }
+    // console.log(hash)
+    for (const prop in hash) {
+        if (hash[prop] === -1 || isNaN(hash[prop])) {
+            return prop
+        }
+    }
+};

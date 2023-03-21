aba7b660dc0559c46f4aaf337de4d4be4639e37e
Zach Seever <51473778+zseever@users.noreply.github.com>
2023-03-20 23:55:14 -0700

    Time: 67 ms (43.45%), Space: 42.1 MB (27.04%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.js
+++ 0367-valid-perfect-square/0367-valid-perfect-square.js
@@ -11,24 +11,19 @@ var isPerfectSquare = function(num) {
     // return false
     let counter = 0
     let start = 0
-    // let end = Math.ceil(num/2)
-    let end = num
+    let end = Math.ceil(num/2)
+    // let end = num
     let mid = Math.ceil((start + end)/2)
     while (start !== end) {
-    // while (counter < 10) {
         if (mid * mid === num) {
             console.log(start,' ',mid,' ',end)
             return true
         } else if (mid * mid > num) {
-            // console.log('mid*mid greater than num')
             end = mid - 1
         } else {
-            // console.log('mid*mid less than num')
             start = mid
         }
         mid = Math.ceil((start + end)/2)
-        counter++
-        // console.log(start,' ',mid,' ',end)
     }
     if (mid * mid === num) {
             console.log(start,' ',mid,' ',end)

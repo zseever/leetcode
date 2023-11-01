cbb976327bb454832b21b62269ddba4cd4661408
Zach Seever <51473778+zseever@users.noreply.github.com>
2023-11-01 06:06:46 -0700

    Time: 530 ms (5.65%), Space: 95.6 MB (5.01%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -2,28 +2,95 @@
  * @param {string} s
  * @return {number}
  */
+
 var lengthOfLongestSubstring = function(s) {
-    s = s.split('')
-    let left = 0
-    let right = 0
-    let longestSubStr = 0
-    let curSubStrLen = 0
-    let prevSeen = {}
-    while (longestSubStr < s.length - left) {
-        let curChar = (s[right] === ' ') ? 'space' : s[right]
-        if (prevSeen[curChar] >= left || !curChar) {
-            left = prevSeen[curChar] + 1
-            prevSeen[curChar] = right
+    if (s.length === 0) {
+        return 0
+    }
+    let left = 0 
+    let right = 1
+    let maxStr = ''
+    let tempStr = ''
+    let usedChars = {}
+    while (left < s.length) {
+        let subStr = s.slice(left,right)
+        if (!usedChars[s[right-1]]) {
+            if (subStr.length > maxStr.length) {
+                maxStr = subStr
+            }
+            usedChars[s[right-1]] = true
             right++
-            curSubStrLen = right - left         
-        } else {
-            prevSeen[curChar] = right
-            curSubStrLen++
-            right++
-        }
-        if (longestSubStr < curSubStrLen) {
-            longestSubStr = curSubStrLen
+        } else if (usedChars[s[right-1]]) {
+            usedChars = {}
+            left++
+            right = left + 1
         }
     }
-    return longestSubStr
-};
+    return maxStr.length
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var lengthOfLongestSubstring = function(s) {
+//     s = s.split('')
+//     let left = 0
+//     let right = 0
+//     let longestSubStr = 0
+//     let curSubStrLen = 0
+//     let prevSeen = {}
+//     while (longestSubStr < s.length - left) {
+//         let curChar = (s[right] === ' ') ? 'space' : s[right]
+//         if (prevSeen[curChar] >= left || !curChar) {
+//             left = prevSeen[curChar] + 1
+//             prevSeen[curChar] = right
+//             right++
+//             curSubStrLen = right - left         
+//         } else {
+//             prevSeen[curChar] = right
+//             curSubStrLen++
+//             right++
+//         }
+//         if (longestSubStr < curSubStrLen) {
+//             longestSubStr = curSubStrLen
+//         }
+//     }
+//     return longestSubStr
+// };

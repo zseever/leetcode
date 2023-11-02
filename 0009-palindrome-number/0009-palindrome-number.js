7a624ca31cbef0d07a2f3646c363399d4ce58f91
Zach Seever <51473778+zseever@users.noreply.github.com>
2023-11-02 03:01:37 -0700

    Time: 152 ms (28.72%), Space: 50.7 MB (76.03%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.js
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -2,6 +2,64 @@
  * @param {number} x
  * @return {boolean}
  */
+
+
+var isPalindrome = function(x) {
+    x = x.toString()
+    let leftStr = ''
+    let rightStr = ''
+    let left = 0
+    let right = x.length-1
+    while (left < right) {
+        if (x[left] !== x[right]) {
+            return false
+        } else {
+            left++
+            right--
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 var isPalindrome = function(x) {
     x = x.toString()
     let y = x.split('').reverse().join('');

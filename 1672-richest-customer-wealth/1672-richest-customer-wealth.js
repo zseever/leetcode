0f08f2782fb0e7e0ea875205eaeab57d9cfb5b3e
Zach Seever <51473778+zseever@users.noreply.github.com>
2024-02-01 05:37:25 -0800

    Time: 60 ms (22.15%), Space: 49.2 MB (7.33%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.js
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.js
@@ -2,6 +2,67 @@
  * @param {number[][]} accounts
  * @return {number}
  */
+
+
+var maximumWealth = function(accounts) {
+    let topWealth = 0;
+    let wealth = 0;
+    for (let i = 0; i < accounts.length; i++) {
+        for (let j = 0; j < accounts[i].length ; j++) {
+            if (j === 0) {
+                wealth = accounts[i][j]
+            } else {
+                wealth += accounts [i][j]
+            }
+        }
+        if (wealth > topWealth) {
+            topWealth = wealth
+        }
+    }
+    return topWealth
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 var maximumWealth = function(accounts) {
     let topWealth = 0
     for (let i = 0; i < accounts.length; i++) {
@@ -13,3 +74,4 @@ var maximumWealth = function(accounts) {
     }
     return topWealth
 };
+*/

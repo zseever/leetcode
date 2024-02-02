a5f2066061f2d30eff4649b0aa5182b0b2af012b
Zach Seever <51473778+zseever@users.noreply.github.com>
2024-02-02 04:24:52 -0800

    Time: 58 ms (29.24%), Space: 48.9 MB (6.62%) - LeetHub

---
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var numberOfSteps = function(num) {
+    let counter = 0
+    while (num > 0) {
+        if (num % 2 === 0) {
+            num = num / 2
+            counter++
+        } else {
+            num -= 1
+            counter++
+        }
+    }
+    return counter
+};

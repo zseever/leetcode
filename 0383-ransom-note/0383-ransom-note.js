5225a261638ad5a6d6c43852f2c00527d47bc56f
Zach Seever <51473778+zseever@users.noreply.github.com>
2024-02-03 05:38:38 -0800

    Time: 86 ms (32.22%), Space: 53.4 MB (8.97%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.js
+++ 0383-ransom-note/0383-ransom-note.js
@@ -5,16 +5,76 @@
  */
 var canConstruct = function(ransomNote, magazine) {
     let hash = {}
-    ransomNote = ransomNote.split('')
-    magazine.split('').forEach(letter => {
-        hash[letter] ? hash[letter]++ : hash[letter] = 1
-    })
+    for (let i = 0; i < magazine.length; i ++) {
+        let magLetter = magazine[i]
+        hash[magLetter] = hash[magLetter] ? hash[magLetter] + 1 : 1
+    }
+    console.log(hash)
     for (let i = 0; i < ransomNote.length; i++) {
-        if (hash[ransomNote[i]]) {
-            hash[ransomNote[i]]--
+        let ranLetter = ransomNote[i]
+        if (hash[ranLetter]) {
+            // hash[ranLetter] = hash[ranLetter]--
+            hash[ranLetter]--
         } else {
             return false
         }
     }
     return true
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//     let hash = {}
+//     ransomNote = ransomNote.split('')
+//     magazine.split('').forEach(letter => {
+//         hash[letter] ? hash[letter]++ : hash[letter] = 1
+//     })
+//     for (let i = 0; i < ransomNote.length; i++) {
+//         if (hash[ransomNote[i]]) {
+//             hash[ransomNote[i]]--
+//         } else {
+//             return false
+//         }
+//     }
+//     return true
+// };

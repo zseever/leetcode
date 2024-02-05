175f454fe5c002660eccae512e864bd0e1e1539c
Zach Seever <51473778+zseever@users.noreply.github.com>
2024-02-05 05:27:42 -0800

    Time: 46 ms, Memory: 48.9 MB - LeetHub

---
+++ remove-element/remove-element.js
@@ -0,0 +1,42 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === val) {
+            nums.splice(i,1)
+            i--
+        }
+    }
+    return nums.length
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//for (let i = 0; i < nums.length; i++) {
+    //     if (nums[i] === val) {
+    //         nums.splice(i,1)
+    //         i--
+    //     }
+    // }
+    // return nums.length

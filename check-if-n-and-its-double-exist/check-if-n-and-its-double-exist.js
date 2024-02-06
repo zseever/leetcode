3b0e799b3ade4b96f38b20d3c3e897152049af69
Zach Seever <51473778+zseever@users.noreply.github.com>
2024-02-06 06:11:57 -0800

    Time: 48 ms, Memory: 51.3 MB - LeetHub

--- check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.js
+++ check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.js
@@ -5,8 +5,7 @@
 var checkIfExist = function(arr) {
     let hashMap = {}
     for (let i = 0; i < arr.length; i++) {
-        if ((hashMap[arr[i]/2] >= 0 || hashMap[arr[i]*2] >= 0)
-           && hashMap[arr[i]/2] != i) {
+        if (hashMap[arr[i]/2] >= 0 || hashMap[arr[i]*2] >= 0) {
             return true
         }
         hashMap[arr[i]] = i

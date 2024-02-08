0dfd146093782cb784fa8a2d0c05867e8aa0dcf9
Zach Seever <51473778+zseever@users.noreply.github.com>
2024-02-08 01:47:43 -0800

    Time: 57 ms, Memory: 49 MB - LeetHub

--- height-checker/height-checker.js
+++ height-checker/height-checker.js
@@ -4,8 +4,6 @@
  */
 var heightChecker = function(heights) {
     let expected = [...heights].sort((a,b) => { return a-b })
-    console.log(expected)
-    console.log(heights)
     let counter = 0
     for (let i = 0; i < heights.length; i++) {
         if (heights[i] !== expected[i]) {

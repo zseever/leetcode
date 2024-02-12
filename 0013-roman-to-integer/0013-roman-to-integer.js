1e9c6cf3f134e30371a3798447b860efdb951956
Zach Seever <51473778+zseever@users.noreply.github.com>
2024-02-11 23:11:36 -0800

    Time: 120 ms (36.65%), Space: 54.4 MB (31.34%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.js
+++ 0013-roman-to-integer/0013-roman-to-integer.js
@@ -17,38 +17,6 @@ var romanToInt = function(s) {
             total += rLkup[s[i]]
         }
     }
-    
-    
-    
-    // for (let i = 0; i < s.length; i++) {
-    //     if (s[i] === 'I' && (s[i+1] === 'V' || s[i+1] === 'X')) {
-    //         if (s[i+1] === 'V') {
-    //             total += 4
-    //             i++
-    //         } else if (s[i+1] === 'X') {
-    //             total += 9
-    //             i++
-    //         }
-    //     } else if (s[i] === 'X' && (s[i+1] === 'L' || s[i+1] === 'C')) {
-    //         if (s[i+1] === 'L') {
-    //             total += 40
-    //             i++
-    //         } else if (s[i+1] === 'C') {
-    //             total += 90
-    //             i++
-    //         }
-    //     } else if (s[i] === 'C' && (s[i+1] === 'D' || s[i+1] === 'M')) {
-    //         if (s[i+1] === 'D') {
-    //             total += 400
-    //             i++
-    //         } else if (s[i+1] === 'M') {
-    //             total += 900
-    //             i++
-    //         }
-    //     } else {
-    //         total += rLkup[s[i]]
-    //     }
-    // }
     return total
 }
 

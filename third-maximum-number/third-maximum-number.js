1605aad2bbb178a5f086f891008a966860031d4b
Zach Seever <51473778+zseever@users.noreply.github.com>
2024-03-01 01:35:50 -0800

    Time: 60 ms, Memory: 51.5 MB - LeetHub

--- third-maximum-number/third-maximum-number.js
+++ third-maximum-number/third-maximum-number.js
@@ -3,10 +3,8 @@
  * @return {number}
  */
 var thirdMax = function(nums) {
-    // nums.sort((a,b) => b-a)
     let set = [...new Set(nums)]
     let num1, num2, num3
-    // num1 = num2 = num3
     for (let i = 0; i< set.length; i++) {
         if (set[i] > num1 || num1 === undefined) {
             num3 = num2
@@ -19,12 +17,6 @@ var thirdMax = function(nums) {
             num3 = set[i]
         }
     }
-    if (set.length < 3) {
-        return num1
-    } else {
-        return num3
-    }
     
-    
-    // return (set.length < 3) ? set[0] : set[2]
+    return (set.length < 3) ? num1 : num3
 };

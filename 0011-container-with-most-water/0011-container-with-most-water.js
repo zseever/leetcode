bf5ace3037b77255aba9d9eda49be3bc2a501de3
Zach Seever <51473778+zseever@users.noreply.github.com>
2024-03-10 05:00:16 -0700

    Time: 1316 ms (5.00%), Space: 71.1 MB (5.01%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+    let left = 0
+    let right = height.length-1
+    let maxArea = 0
+    while (left !== right) {
+        let area = (right-left) * (Math.min(height[left],height[right]))
+        console.log('area: '+area+',left: '+left+',right: '+right)
+        if (area > maxArea) {
+            maxArea = area
+        }
+        if (height[left] > height[right]) {
+            right--
+        } else {
+            left++
+        }
+    }
+    
+    
+    return maxArea
+};

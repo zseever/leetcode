4e6beb18d5afaa4b47049014c537563176e903a2
Zach Seever <51473778+zseever@users.noreply.github.com>
2024-03-14 05:33:09 -0700

    Time: 56 ms (94.48%), Space: 51.1 MB (77.91%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.js
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.js
@@ -4,17 +4,38 @@
  * @return {number}
  */
 var numSubarraysWithSum = function(nums, goal) {
-    let subCtr = 0
-    for (let left = 0; left < nums.length; left++) {
-        let right = left
+    function helper(x) {
+        if (x < 0) return 0
+        
+        let res = 0
+        let l = 0
         let sum = 0
-        while (sum <= goal && right < nums.length) {
-            sum += nums[right]
-            right++
-            if (sum === goal) {
-                subCtr++  
+        
+        for (let r=0; r < nums.length; r++) {
+            sum += nums[r]
+            while (sum > x) {
+                sum -= nums[l]
+                l += 1
             }
+            res += (r - l + 1)
         }
+        return res
     }
-    return subCtr
+    
+    
+    return helper(goal) - helper(goal-1)
+    
+    // let subCtr = 0
+    // for (let left = 0; left < nums.length; left++) {
+    //     let right = left
+    //     let sum = 0
+    //     while (sum <= goal && right < nums.length) {
+    //         sum += nums[right]
+    //         right++
+    //         if (sum === goal) {
+    //             subCtr++  
+    //         }
+    //     }
+    // }
+    // return subCtr
 };

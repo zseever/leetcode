9469a815d927e4fc5a6fb623432ea7afab49b006
Zach Seever <51473778+zseever@users.noreply.github.com>
2024-03-14 23:27:29 -0700

    Time: 86 ms (63.07%), Space: 61.3 MB (41.27%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.js
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -3,33 +3,27 @@
  * @return {number[]}
  */
 var productExceptSelf = function(nums) {
+    let n = nums.length
     let prefix = []
-    let postfix = new Array(nums.length).fill(1)
+    let postfix = new Array(n).fill(1)
     let result = []
     let product = 1
     let product2 = 1
-    for (let i=0; i < nums.length; i++) {
+    for (let i=0; i < n; i++) {
         product *= nums[i]
         prefix.push(product)      
         
-        product2 *= nums[nums.length-1-i]
-        postfix[nums.length-1-i] = product2
+        product2 *= nums[n-1-i]
+        postfix[n-1-i] = product2
     }
-    // product = 1
-    // for (let i=nums.length-1; i >= 0; i--) {
-    //     product *= nums[i]
-    //     postfix[i] = product
-    // }
-    for (let i=0; i < nums.length; i++) {
-        let val
+    for (let i=0; i < n; i++) {
         if (i === 0) {
-            val = 1 * postfix[i+1]
-        } else if (i === nums.length-1) {
-            val = prefix[i-1] * 1
+            result.push(1 * postfix[i+1])
+        } else if (i === n-1) {
+            result.push(val = prefix[i-1] * 1)
         } else {
-            val = prefix[i-1] * postfix[i+1]
+            result.push(val = prefix[i-1] * postfix[i+1])
         }
-        result.push(val)
     }
     
     

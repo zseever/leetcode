e872ebe884ac7605dfbdb281eae86fb48e13d9a4
Zach Seever <51473778+zseever@users.noreply.github.com>
2024-03-14 23:23:41 -0700

    Time: 88 ms (55.64%), Space: 60.9 MB (52.44%) - LeetHub

---
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var productExceptSelf = function(nums) {
+    let prefix = []
+    let postfix = new Array(nums.length).fill(1)
+    let result = []
+    let product = 1
+    for (let i=0; i < nums.length; i++) {
+        product *= nums[i]
+        prefix.push(product)        
+    }
+    product = 1
+    for (let i=nums.length-1; i >= 0; i--) {
+        product *= nums[i]
+        postfix[i] = product
+    }
+    for (let i=0; i < nums.length; i++) {
+        let val
+        if (i === 0) {
+            val = 1 * postfix[i+1]
+        } else if (i === nums.length-1) {
+            val = prefix[i-1] * 1
+        } else {
+            val = prefix[i-1] * postfix[i+1]
+        }
+        result.push(val)
+    }
+    
+    
+    return result
+};

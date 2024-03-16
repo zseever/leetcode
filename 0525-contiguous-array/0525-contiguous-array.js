6161359faa6c5a2a1dcd0591d43e80569d0d69bc
Zach Seever <51473778+zseever@users.noreply.github.com>
2024-03-16 00:10:02 -0700

    Time: 133 ms (9.09%), Space: 58.3 MB (37.66%) - LeetHub

--- 0525-contiguous-array/0525-contiguous-array.js
+++ 0525-contiguous-array/0525-contiguous-array.js
@@ -3,12 +3,16 @@
  * @return {number}
  */
 var findMaxLength = function(nums) {
+    // keep track of zeroes and ones as we iterate
+    // for each iteration, store the diff btwn zeroes and ones at an index if
+    //      it does not exist already in a hash map
+    // for each iteration, if zeroes = ones then we know it is the largest current value
+    // if zeroes != ones, we want to check our hashmap to see if there was a subarr with the same               diff as current. we check our current index minus the index where that exists to see if             that is larger than our current max length    
     let zeroes = 0
     let ones = 0
     let maxLen = 0
     let diff_idx = {}
-    
-    
+        
     for (let i=0; i < nums.length; i++) {
         if (nums[i] === 1) {
             ones++
@@ -26,12 +30,6 @@ var findMaxLength = function(nums) {
             let idx = diff_idx[zeroes-ones]
             maxLen = Math.max(maxLen, i-idx)
         }
-    }
-    
-    return maxLen
-    
+    }  
+    return maxLen    
 };
-
-// [0,0,1,0,1,1,0]
-// zero: 3
-// one: 4

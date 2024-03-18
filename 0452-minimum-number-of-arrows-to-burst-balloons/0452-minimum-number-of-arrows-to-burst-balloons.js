9882c97b8377d150ecd8096397b59c3ba1265198
Zach Seever <51473778+zseever@users.noreply.github.com>
2024-03-18 01:57:45 -0700

    Time: 1208 ms (5.07%), Space: 76.3 MB (5.18%) - LeetHub

---
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[][]} points
+ * @return {number}
+ */
+var findMinArrowShots = function(points) {
+    if (points.length === 1) return 1
+    points.sort((subArrA, subArrB) => {
+        if (subArrA[0] > subArrB) {
+            return 1
+        } else if (subArrA[0] < subArrB[0]) {
+            return -1
+        } else {
+            return 0
+        }
+    })
+    console.log(points)
+    let curVal
+    let intervals = 0
+    for (let i=0; i < points.length; i++) {
+        curVal = points[i][1]
+        let j = i+1
+        while (j < points.length && curVal >= points[j][0]) {
+            curVal = Math.min(curVal, points[j][1])
+            j++
+        }
+            i = j-1
+            intervals++
+    }
+    return intervals
+};

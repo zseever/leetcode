385cc3e0719e6c5da394382bc566c8cf71cabc76
Zach Seever <51473778+zseever@users.noreply.github.com>
2024-03-19 03:26:29 -0700

    Time: 90 ms (63.82%), Space: 53.5 MB (74.12%) - LeetHub

---
+++ 0621-task-scheduler/0621-task-scheduler.js
@@ -0,0 +1,39 @@
+/**
+ * @param {character[]} tasks
+ * @param {number} n
+ * @return {number}
+ */
+var leastInterval = function(tasks, n) {
+    let hash = {}
+    let newArr = []
+    for (let i=0; i < tasks.length; i++) {
+        hash[tasks[i]] = hash[tasks[i]] ? hash[tasks[i]]+1 : 1
+    }
+    for (const prop in hash) {
+        newArr.push(hash[prop])
+    }
+    newArr.sort((a,b) => a-b)
+    let maxVal = newArr[newArr.length-1] - 1
+    let idle = maxVal * n
+
+    for (let i=newArr.length-2; i>=0; i--) {
+        idle -= Math.min(maxVal, newArr[i])
+    }
+    console.log(idle)
+    return idle > 0 ? idle + tasks.length : tasks.length
+
+};
+
+
+//     let head
+//     let curNode
+//     for (const prop in hash) {
+//         if (!head) {
+//             head = {val: [prop, hash[prop]],
+//                     next: null
+//                    }
+//             curNode = head
+//         } else {
+            
+//         }
+//     }

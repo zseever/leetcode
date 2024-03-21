7e26c16ff2e9bcef094544f3f1b1eb5337dec8c1
Zach Seever <51473778+zseever@users.noreply.github.com>
2024-03-20 18:13:24 -0700

    Time: 76 ms (84.11%), Space: 62.5 MB (51.04%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.js
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.js
@@ -4,23 +4,43 @@
  * @return {number}
  */
 var findMaxAverage = function(nums, k) {
-    let rollingTotal = 0
-    let maxAverage = undefined
-    let counter = 0
-    let hash = {}
-    while (counter < k-1) {
-        hash[counter] = nums[counter]
-        rollingTotal += nums[counter]
-        counter++
+    let left = 0
+    let right = 0
+    let runningTotal = 0
+    let maxAvg = null
+    while (right < k-1) {
+        runningTotal += nums[right]
+        right++
     }
-    for (let i=counter; i < nums.length; i++) {
-        hash[i] = nums[i]
-        rollingTotal += nums[i]
-        let average = rollingTotal / k
-        if (maxAverage === undefined || average > maxAverage) {
-            maxAverage = average
-        }
-        rollingTotal -= hash[i-(k-1)]  
+    while (left <= nums.length-k) {
+        runningTotal += nums[right]
+        let avg = runningTotal / k
+        maxAvg = avg > maxAvg || maxAvg === null ? avg : maxAvg
+        runningTotal -= nums[left]
+        left++
+        right++
     }
-    return maxAverage
+    return maxAvg
+    
+    
+    
+    // let rollingTotal = 0
+    // let maxAverage = undefined
+    // let counter = 0
+    // let hash = {}
+    // while (counter < k-1) {
+    //     hash[counter] = nums[counter]
+    //     rollingTotal += nums[counter]
+    //     counter++
+    // }
+    // for (let i=counter; i < nums.length; i++) {
+    //     hash[i] = nums[i]
+    //     rollingTotal += nums[i]
+    //     let average = rollingTotal / k
+    //     if (maxAverage === undefined || average > maxAverage) {
+    //         maxAverage = average
+    //     }
+    //     rollingTotal -= hash[i-(k-1)]  
+    // }
+    // return maxAverage
 };

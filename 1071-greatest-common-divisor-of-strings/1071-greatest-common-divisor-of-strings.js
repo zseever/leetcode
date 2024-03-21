c62699a7f59c22382000bde2ffc8b7b5cceb8ecb
Zach Seever <51473778+zseever@users.noreply.github.com>
2024-03-20 17:44:44 -0700

    Time: 46 ms (89.29%), Space: 49.9 MB (39.56%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.js
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.js
@@ -4,24 +4,28 @@
  * @return {string}
  */
 var gcdOfStrings = function(str1, str2) {
-    let len1 = str1.length // 6
-    let len2 = str2.length // 3
+    let len1 = str1.length 
+    let len2 = str2.length 
     function isDivisor(num1, num2) {
         return num1 % num2 === 0 
     }
     for (let i = Math.min(len1, len2); i >= 0; i--) {
         if (isDivisor(len1, i) && isDivisor(len2,i)) {
-            if (len1 >= len2) {
-                let subStr = str2.slice(0,i)
+            let subStr = len1 >= len2 ? str2.slice(0,i) : str1.slice(0,i)
                 if (subStr.repeat(len1/i) === str1 && subStr.repeat(len2/i) === str2) {
                     return subStr
                 }
-            } else if (len2 > len1) {
-                let subStr = str1.slice(0,i)
-                if (subStr.repeat(len1/i) === str1 && subStr.repeat(len2/i) === str2) {
-                    return subStr
-                }
-            }
+            // if (len1 >= len2) {
+            //     let subStr = str2.slice(0,i)
+            //     if (subStr.repeat(len1/i) === str1 && subStr.repeat(len2/i) === str2) {
+            //         return subStr
+            //     }
+            // } else if (len2 > len1) {
+            //     let subStr = str1.slice(0,i)
+            //     if (subStr.repeat(len1/i) === str1 && subStr.repeat(len2/i) === str2) {
+            //         return subStr
+            //     }
+            // }
         }   
     }
     return ''

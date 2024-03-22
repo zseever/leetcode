2198e4010d930b8e3f1dae7b3ab5fda411478313
Zach Seever <51473778+zseever@users.noreply.github.com>
2024-03-21 19:31:45 -0700

    Time: 113 ms (85.90%), Space: 70.2 MB (74.19%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.js
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.js
@@ -10,15 +10,48 @@
  * @return {boolean}
  */
 var isPalindrome = function(head) {
-    let val = ''
-    let curNode = head
+    let fast = head
+    let slow = head
+    let prev
+    let temp
     
-    while (curNode) {
-        val += curNode.val
-        curNode = curNode.next
+    while (fast && fast.next) {
+        slow = slow.next
+        fast = fast.next.next
     }
-    let half1 = val.slice(0, Math.floor(val.length/2))
-    let half2 = val.slice(Math.ceil(val.length/2), val.length).split('').reverse().join('')
     
-    return half1 === half2
+    prev = slow
+    slow = slow.next
+    prev.next = null
+    
+    while (slow) {
+        temp = slow.next
+        slow.next = prev
+        prev = slow
+        slow = temp
+    }
+    
+    fast = head
+    slow = prev
+    
+    while (slow) {
+        if (fast.val !== slow.val) return false
+        
+        slow = slow.next
+        fast = fast.next
+    }
+    
+    return true
+    
+//     let val = ''
+//     let curNode = head
+    
+//     while (curNode) {
+//         val += curNode.val
+//         curNode = curNode.next
+//     }
+//     let half1 = val.slice(0, Math.floor(val.length/2))
+//     let half2 = val.slice(Math.ceil(val.length/2), val.length).split('').reverse().join('')
+    
+//     return half1 === half2
 };

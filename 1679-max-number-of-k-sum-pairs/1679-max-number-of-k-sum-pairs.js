e91e53b7e19ada3721f3f6271f5be7328d58674d
Zach Seever <51473778+zseever@users.noreply.github.com>
2024-03-28 03:16:11 -0700

    Time: 523 ms (5.02%), Space: 82.5 MB (5.03%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.js
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.js
@@ -4,25 +4,37 @@
  * @return {number}
  */
 var maxOperations = function(nums, k) {
-    nums.sort((a,b) => a-b) 
-    let result = 0
-    let left = 0
-    let right = nums.length - 1
+    let seen = {}
+    let res = 0
     
-    while (left < right) {
-        let sum = nums[left]+nums[right]
-        if (sum < k) {
-            left++
-        } else if (sum > k) {
-            right--
+    for (let i=0; i < nums.length; i++) {
+        let tar = k-nums[i]
+        if (seen[tar] === undefined) {
+            seen[tar] = [i]
         } else {
-            result++
-            left++
-            right--
+            seen[tar].push(i)
+        }
+    }   
+    
+    console.log(seen)
+    
+    for (let i=0; i < nums.length; i++) {
+        let tar = k-nums[i]
+        if (seen[tar]?.length >= 1 && seen[nums[i]]?.length >= 1) {
+            if (tar === nums[i] && seen[tar].length === 1) {
+                console.log('criteria met')
+            } else {
+                res++
+                seen[tar].pop()
+                seen[nums[i]].pop()  
+            }
+            //{ '2': [ 3 ], '3': [ 0, 2, 4 ], '5': [ 1 ] }
+
+            
         }
     }
-    return result
+    
+    return res
+    
+    
 };
-
-// 1, 3, 3, 3, 4
-// 7 -> 

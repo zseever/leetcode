a026242290ef351fbee76c3661ffec5b91cd7cd4
Zach Seever <51473778+zseever@users.noreply.github.com>
2024-03-28 00:02:31 -0700

    Time: 145 ms (85.84%), Space: 63.9 MB (89.38%) - LeetHub

--- 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.js
+++ 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.js
@@ -9,18 +9,17 @@ var maxSubarrayLength = function(nums, k) {
     let right = 0
     let res = 0
     
-    while (left < nums.length) {
+    while (right < nums.length) {
+        hash[nums[right]] = (hash[nums[right]] || 0) + 1
         
-        if (right < nums.length && (hash[nums[right]] < k || hash[nums[right]] === undefined)) {
-            hash[nums[right]] = hash[nums[right]] >= 1 ? hash[nums[right]] + 1 : 1
-            res = Math.max(res, (right-left)+1)
-            right++
-        } else {
-            hash[nums[left]] = hash[nums[left]] > 0 ? hash[nums[left]] - 1 : 0
+        while (hash[nums[right]] > k) {
+            hash[nums[left]]--
             left++
-
         }
-
+        
+        res = Math.max(res, (right-left)+1)
+        right++
+        
     }
-    return res === 0 ? 1 : res
+    return res
 };

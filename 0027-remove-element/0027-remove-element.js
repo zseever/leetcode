ffae38b4b93242bfea423f2affeea85cafce1e3e
Zach Seever <51473778+zseever@users.noreply.github.com>
2024-04-03 06:18:51 -0700

    Time: 55 ms (46.17%), Space: 48.4 MB (91.06%) - LeetHub

--- 0027-remove-element/0027-remove-element.js
+++ 0027-remove-element/0027-remove-element.js
@@ -12,3 +12,31 @@ var removeElement = function(nums, val) {
     }
     return nums.length
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//for (let i = 0; i < nums.length; i++) {
+    //     if (nums[i] === val) {
+    //         nums.splice(i,1)
+    //         i--
+    //     }
+    // }
+    // return nums.length

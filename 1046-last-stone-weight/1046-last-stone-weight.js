24913b041cfc1f769f814741b88471d10adab724
Zach Seever <51473778+zseever@users.noreply.github.com>
2024-05-15 03:54:17 -0700

    Time: 58 ms (53.59%), Space: 48.4 MB (98.06%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.js
+++ 1046-last-stone-weight/1046-last-stone-weight.js
@@ -22,28 +22,18 @@ var lastStoneWeight = function(stones) {
                 min = i
             }
         }
-        console.log(min,'-',max)
         let result = stones[min]-stones[max]
+        
         if (result < 0) {
-            // max is greater than min
-            // update stones[max] to abs(result)
-            // delete stones[min]
             stones[max] =  Math.abs(result)
             stones.splice(min,1)
         } else if (result > 0) {
-            // min is greater than max
-            // update stones[min] to result
-            // delete stones[max]
             stones[min] = result
             stones.splice(max,1)
         } else {
-            // result is 0
-            // delete stones[max]
-            // delete stones[min]
             stones.splice(Math.max(min,max),1)
             stones.splice(Math.min(min,max),1)
         }
-        console.log(stones)
     }
     
     return stones[0] > 0 ? stones[0] : 0

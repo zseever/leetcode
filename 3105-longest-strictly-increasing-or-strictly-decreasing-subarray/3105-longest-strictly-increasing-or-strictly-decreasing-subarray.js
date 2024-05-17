726bf55acab31e92563b1eaac842795432b48496
Zach Seever <51473778+zseever@users.noreply.github.com>
2024-05-17 05:56:22 -0700

    Time: 59 ms (76.34%), Space: 50.6 MB (84.59%) - LeetHub

--- 3105-longest-strictly-increasing-or-strictly-decreasing-subarray/3105-longest-strictly-increasing-or-strictly-decreasing-subarray.js
+++ 3105-longest-strictly-increasing-or-strictly-decreasing-subarray/3105-longest-strictly-increasing-or-strictly-decreasing-subarray.js
@@ -6,33 +6,18 @@ var longestMonotonicSubarray = function(nums) {
     let res = 1
     let left = 0
     let right = 0
-    let direction
+    let dir
     
     while (left < nums.length) {
-        direction = nums[left+1] - nums[left]
+        dir = nums[left+1] - nums[left]
         
-        while ((direction > 0 && nums[right+1] > nums[right]) || (direction < 0 && nums[right+1] < nums[right]))  {
+        while ((dir > 0 && nums[right+1] > nums[right]) || (dir < 0 && nums[right+1] < nums[right]))  {
             if ((right-left)+2 > res) {
                 res = (right-left)+2
             }
             right++        
         }  
 
-        // if (direction > 0) {
-        //     while (nums[right+1] > nums[right]) {
-        //         if ((right-left)+2 > res) {
-        //             res = (right-left)+2
-        //         }
-        //         right++        
-        //     }  
-        // } else if (direction < 0) {
-        //     while (nums[right+1] < nums[right]) {
-        //         if ((right-left)+2 > res) {
-        //             res = (right-left)+2
-        //         }
-        //         right++
-        //     }              
-        // }
         left += 1
         right = left
         

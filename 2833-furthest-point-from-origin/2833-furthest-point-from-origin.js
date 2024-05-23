0acf125afb14afc1ce023f609896918f2d1d6551
Zach Seever <51473778+zseever@users.noreply.github.com>
2024-05-23 02:50:36 -0700

    Time: 81 ms (40.96%), Space: 53.2 MB (71.08%) - LeetHub

--- 2833-furthest-point-from-origin/2833-furthest-point-from-origin.js
+++ 2833-furthest-point-from-origin/2833-furthest-point-from-origin.js
@@ -7,16 +7,8 @@ var furthestDistanceFromOrigin = function(moves) {
     let loop2 = 0
     
     for (let i=0; i < moves.length; i++) {
-        if (moves[i] === 'L') {
-            loop1 -= 1
-            loop2 -= 1
-        } else if (moves[i] === 'R') {
-            loop1 += 1
-            loop2 += 1
-        } else {
-            loop1 += 1
-            loop2 -= 1
-        }
+        loop1 += moves[i] === 'L' ? -1 : 1
+        loop2 += moves[i] === 'R' ? 1 : -1
     }
     
     return Math.max(Math.abs(loop2), Math.abs(loop1))

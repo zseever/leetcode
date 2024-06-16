92c25461010484514417603cbd132d55c5d9c5f9
Zach Seever <51473778+zseever@users.noreply.github.com>
2024-06-15 22:13:10 -0700

    Time: 189 ms (5.00%), Space: 59 MB (5.00%) - LeetHub

---
+++ 1103-distribute-candies-to-people/1103-distribute-candies-to-people.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number} candies
+ * @param {number} num_people
+ * @return {number[]}
+ */
+var distributeCandies = function(candies, num_people) {
+    let result = new Array(num_people)
+    result.fill(0)
+    let counter = 0
+    
+    while (candies > 0) {
+        let base = counter === 0 ? 1 : (counter*num_people)+1
+        for (let i=0; i < num_people; i++) {
+            if (candies < base + i) {
+                result[i] += candies
+                candies = 0
+                break
+            } else {
+                result[i] += base + i
+                candies -= base + i
+                console.log(result[i],'-',candies)
+            }
+        }
+        counter++
+    }
+    
+    return result
+};
+
+// 1,2,3 - 9,7,4
+// 5,2,3 - 0,-,-
